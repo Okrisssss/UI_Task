8d8346600ccf89dd8dfc3278509608b40d1e053c
piachimov <Petru.Iachimov@endava.com>
2018-10-24 16:24:34 +0300

    'initial_commit'

---
+++ app/src/main/java/com/example/piachimov/android_ui/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.piachimov.android_ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.btnGoToUiActivity)
+    Button btnGoToUiActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btnGoToUiActivity})
+    public void goToUiActivity(){
+        Intent uiIntent = new Intent(this, UserPageActivity.class);
+        startActivity(uiIntent);
+    }
+}
