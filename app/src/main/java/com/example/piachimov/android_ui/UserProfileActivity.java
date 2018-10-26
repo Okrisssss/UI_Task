0aea503cff705c637037e3c25db7b782efce3914
piachimov <Petru.Iachimov@endava.com>
2018-10-26 13:04:08 +0300

    'final_update'

--- app/src/main/java/com/example/piachimov/android_ui/UserProfileActivity.java
+++ app/src/main/java/com/example/piachimov/android_ui/UserProfileActivity.java
@@ -11,11 +11,29 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class UserProfileActivity extends AppCompatActivity {
+    @BindView(R.id.upperToolBar)
+    Toolbar upperToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
         ButterKnife.bind(this);
+        initToolBar();
     }
+
+    public void initToolBar(){
+    setSupportActionBar(upperToolBar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setDisplayShowTitleEnabled(false);
+    getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_tool_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 }
